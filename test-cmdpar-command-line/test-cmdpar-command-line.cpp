60a53d6767a0f229aad1497af2870307526b72bc
Secoh <oalferov1@gmail.com>
2022-07-19 23:19:32 -0700

    updated access paths for cmdpar classes added parameter with UTF-16 name

--- test-cmdpar-command-line/test-cmdpar-command-line.cpp
+++ test-cmdpar-command-line/test-cmdpar-command-line.cpp
@@ -9,14 +9,11 @@
 // published under the same terms as the original one(s), but you don't have to inherit the special exception above.
 //
 
-
 #include <iostream>
 
 #include <SKLib/include/cmdpar.hpp>
 
-
 //  test [-a][-b][-ur] -z<char> -za<char> arg1 arg2 [num1]
-
 SKLIB_DECLARE_CMD_PARAMS(test_list)
 {
     param_switch<char> nnn{ this, "nnn" };
@@ -33,7 +30,9 @@ SKLIB_DECLARE_CMD_PARAMS(test_list)
 
     SKLIB_PLAIN_STRING(r1, "aaa");
     SKLIB_PLAIN_STRING(r2, "bbb");
-    SKLIB_PLAIN_OPT_INT(d1, 987);
+    SKLIB_PLAIN_OPTION_INT(d1, 987);
+
+    SKLIB_OPTION_SWITCH_NAME(palka, L"палка");   // palka in Russian (English: stick)
 
     SKLIB_OPTION_HELP(h);
 
@@ -50,6 +49,8 @@ std::wstring str_AtoW(const std::string& str)
 
 void show_option_status(const char* tstr, const test_list::param_base& param)
 {
+    typedef typename std::decay_t<decltype(param)>::option_flag option_flag;    // "using" equivalent
+
     std::cout << "> ";
 
     auto* s1 = strrchr(tstr, ':');
@@ -57,12 +58,12 @@ void show_option_status(const char* tstr, const test_list::param_base& param)
     else std::cout << tstr;
 
     if (!param.status())                             std::cout << "; No Options";
-    if (param.status() & param.option_required)      std::cout << "; Required";
-    if (param.status() & param.option_present)       std::cout << "; Present";
-    if (param.status() & param.option_is_help)       std::cout << "; Option is Help";
-    if (param.status() & param.option_help_request)  std::cout << "; Help Requested";
-    if (param.status() & param.option_error_empty)   std::cout << "; Error: Empty";
-    if (param.status() & param.option_error_partial) std::cout << "; Error: Partial";
+    if (param.status() & option_flag::required)      std::cout << "; Required";
+    if (param.status() & option_flag::present)       std::cout << "; Present";
+    if (param.status() & option_flag::is_help)       std::cout << "; Option is Help";
+    if (param.status() & option_flag::help_request)  std::cout << "; Help Requested";
+    if (param.status() & option_flag::error_empty)   std::cout << "; Error: Empty";
+    if (param.status() & option_flag::error_partial) std::cout << "; Error: Partial";
     std::cout << "\n";
 }
 
@@ -90,27 +91,31 @@ void show_param(const test_list::param_base& param, const char* tstr, const wcha
 
 void show_parser_status(bool ok, const test_list& root)
 {
+    typedef typename std::decay_t<decltype(root.parser_status)>::flags parser_flag;    // another "using"
+
     std::cout << (ok ? "Parse OK\n" : "Parse Bad\n");
 
-    if (root.status() & root.parser_good)                 std::cout << "Params Valid\n";
-    if (root.status() & root.parser_empty)                std::cout << "Empty Input\n";
-    if (root.status() & root.parser_error)                std::cout << "Error State\n";
-    if (root.status() & root.parser_error_unknown_name)   std::cout << "Error: Unknown Name\n";
-    if (root.status() & root.parser_error_missing_named)  std::cout << "Error: Required Named Parameter Not Present\n";
-    if (root.status() & root.parser_error_missing_plain)  std::cout << "Error: Required Plain Parameter Not Present\n";
-    if (root.status() & root.parser_error_overflow_named) std::cout << "Error: Too Many Named Parameters (Repeats)\n";
-    if (root.status() & root.parser_error_overflow_plain) std::cout << "Error: Too Many Plain Parameters\n";
-    if (root.status() & root.parser_error_malformed)      std::cout << "Error: Malformed Parameter Value\n";
-    if (root.status() & root.parser_help_request)         std::cout << "Help Requested\n";
-    if (root.status() & root.parser_help_banner)          std::cout << "Help: Banner\n";
-    if (root.status() & root.parser_help_option)          std::cout << "Help: On Parameter\n";
+    auto status = root.parser_status.get();
 
-    if (root.status()) std::cout << "\n";
+    if (status & parser_flag::good)                 std::cout << "Params Valid\n";
+    if (status & parser_flag::empty)                std::cout << "Empty Input\n";
+    if (status & parser_flag::error)                std::cout << "Error State\n";
+    if (status & parser_flag::error_unknown_name)   std::cout << "Error: Unknown Name\n";
+    if (status & parser_flag::error_missing_named)  std::cout << "Error: Required Named Parameter Not Present\n";
+    if (status & parser_flag::error_missing_plain)  std::cout << "Error: Required Plain Parameter Not Present\n";
+    if (status & parser_flag::error_overflow_named) std::cout << "Error: Too Many Named Parameters (Repeats)\n";
+    if (status & parser_flag::error_overflow_plain) std::cout << "Error: Too Many Plain Parameters\n";
+    if (status & parser_flag::error_malformed)      std::cout << "Error: Malformed Parameter Value\n";
+    if (status & parser_flag::help_request)         std::cout << "Help Requested\n";
+    if (status & parser_flag::help_banner)          std::cout << "Help: Banner\n";
+    if (status & parser_flag::help_option)          std::cout << "Help: On Parameter\n";
+
+    if (status) std::cout << "\n";
 }
 
 int main(int argn, char* argc[])
 {
-    bool is_ok = PPP.parse(argn, argc);
+    bool is_ok = PPP.parser_run(argn, argc);
 
     show_parser_status(is_ok, PPP);
 
@@ -127,6 +132,10 @@ int main(int argn, char* argc[])
     SHOW_PARAM(PPP.r1);
     SHOW_PARAM(PPP.r2);
     SHOW_PARAM(PPP.d1);
+
+    const auto pflag = PPP.parser_status.flag.good;     // another way to access parser flags
+
+    const auto oflag = decltype(PPP)::param_uint<wchar_t>::param_base::option_flag::is_help;  // example of inheritance chain
 
 }
 
